d28fbbedd312cdd1ab45ca6d43d97a700c62d17e
LDLaura <despesselaura@yahoo.fr>
2024-06-17 16:48:41 +0200

    feat(nav): navbar + banner

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Simplifiez votre vie et n'oubliez plus rien avec TODOLIST">
+    <title>TO DO LIST</title>
+
+    <link rel="stylesheet" href="./style/main.css">
+</head>
+<body>
+    <header class="navbar">
+        <nav class="navbar-content">
+            <img src="./images/logo-notes.png" alt="Ce logo représente une forme oval marron avec écrit dessus Notes" class="navbar-logo">
+            <ul class="navbar-links">
+                <li class="navbar-link">
+                    <a href="#">Mon compte</a>
+                </li>
+                <li class="navbar-link">
+                    <a href="#">S'inscrire</a>
+                </li>
+                <li class="navbar-link">
+                    <a href="#" class="navbar-btn">
+                        Bonus
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section class="banner">
+            <div class="banner-container">
+                <p class="banner-container-content">
+                    BIENVENUE DANS NOTES, DESORMAIS TOUT SERA NOTÉ ICI !
+                </p>
+                <img class="banner-container-img" src="./images/banner-img.jpg" alt="Ceci représente le dessus d'une table  une tasse de café, un pot de crayons et un bloc note avec une to do list ">
+            </div>
+        </section>
+    </main>
+    
+</body>
+</html>
