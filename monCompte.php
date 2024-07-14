ce98bf40a1018e05d97a8023041b8b021469ddb2
LDLaura <despesselaura@yahoo.fr>
2024-07-14 09:53:31 +0200

    feat(moncompte): creation de la page + css

---
+++ monCompte.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Simplifiez votre vie et n'oubliez plus rien avec TODOLIST">
+    <title>TO DO LIST : Mon compte</title>
+
+    <link rel="stylesheet" href="./style/monCompte.css">
+</head>
+<body>
+    <header class="navbar">
+            <nav class="navbar-content">
+                <a href="http://localhost/TODOLIST/"><img src="./images/logo-notes.png" alt="Ce logo représente une forme oval marron avec écrit dessus Notes" class="navbar-logo"></a>
+                <ul class="navbar-links">
+                    <li class="navbar-link">
+                        <a href="#">Ajouter</a>
+                    </li>
+                    <li class="navbar-link">
+                        <a href="#">Modifier</a>
+                </ul>
+            </nav>
+        </header>
+
+        <main>
+            <section class="main-content">
+                <h2>Mes dernières To Do List :</h2>
+            </section>
+            
+        </main>
+    
+</body>
+</html>
