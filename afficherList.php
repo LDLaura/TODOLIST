a3b3219b234f8470033d665fea9476d1f3d40e48
LDLaura <despesselaura@yahoo.fr>
2024-07-19 16:06:06 +0200

    feat(user): afficher liste

--- afficherList.php
+++ afficherList.php
@@ -1,8 +1,8 @@
 <?php
-//Ouvrir la session pour pouvoir récupérer les valeurs sotckées dans $_SESSION
+
 session_start();
 
-//Vérifier que l'utilisateur soit connecté
+//Verifier si l'utilisateur est connecté
 if (!isset($_SESSION['id'])) {
     header('Location: connexion.php');
 }
@@ -10,14 +10,20 @@ if (!isset($_SESSION['id'])) {
 //Import
 require_once 'service/db_connect.php';
 
-$id = $_SESSION['id'];
+//Dans l'url retrouver l'id de la liste
+// Avec $_GET['idListe'] recupérer les infos de la liste
 
-$request = $db_connexion->prepare('SELECT idListe, title, content, createdAt FROM list WHERE idUsers = :id ORDER BY createdAt DESC LIMIT 5');
-$request->bindParam(':id', $id);
-$request->execute();
+$idListe = $_GET['id'];
 
-$resultat = $request->fetchAll(PDO::FETCH_ASSOC);
+$request = 'SELECT title, content FROM list WHERE idListe = :idListe';
+$stmt = $db_connexion->prepare($request);
+$stmt->bindParam(':idListe', $idListe);
 
+$stmt->execute();
+$resultat = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+$title = $resultat[0]['title'];
+$content = $resultat[0]['content'];
 
 ?>
 
@@ -28,9 +34,9 @@ $resultat = $request->fetchAll(PDO::FETCH_ASSOC);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Simplifiez votre vie et n'oubliez plus rien avec TODOLIST">
-    <title>TO DO LIST : Mon compte</title>
+    <title>TO DO LIST : Liste</title>
 
-    <link rel="stylesheet" href="./style/monCompte.css">
+    <link rel="stylesheet" href="./style/afficherList.css">
 </head>
 
 <body>
@@ -42,7 +48,7 @@ $resultat = $request->fetchAll(PDO::FETCH_ASSOC);
                     <a href="ajouterList.php">Ajouter Liste</a>
                 </li>
                 <li class="navbar-link">
-                    <a href="./user/afficherUser.php">Mes infos</a>
+                    <a href="monCompte.php">Mes listes</a>
                 </li>
                 <li class="navbar-link">
                     <a href="#">Déconnexion</a>
@@ -53,43 +59,23 @@ $resultat = $request->fetchAll(PDO::FETCH_ASSOC);
 
     <main>
         <section class="main-content">
-            <h2>Mes dernières To Do List :</h2>
-
-
-            <?php
-            foreach ($resultat as $valeur) {
-                
-            ?>
-                <div class="card">
+            
+            <div class="card">
                     <div class="header">
                         <span class="icon">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
                                 <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clip-rule="evenodd" />
                             </svg>
                         </span>
-                        <?php echo'<a href="afficherList.php?id='.$valeur['idListe'].'" class="title">'. $valeur['title'].' </a>'?>
-                    </div>
-                    <p class="date"><?php $date = date_create($valeur['createdAt']); echo date_format($date, 'd-M-Y'); ?> </p>
-                    <div class="button">
-                        <form action="#" method="POST">
-                            <a href="modifierList.php" class="modify">Modifier</a>
-                        </form>
-                        <form action="supprimerList.php" method="GET">
-                            <input type="hidden" name="idListe" value="<?= $valeur['idListe'] ?>">
-                            <button type="submit" class="delete">Supprimer</button>
-                        </form>
-                    </div>
-
+                        <p class="title"><?= $title ?></p>
+                        </div>
+                        <p class="content"><?= $content ?></p>
                 </div>
-
-            <?php
-            }
-            ?>
-
         </section>
-
     </main>
-
 </body>
 
 </html>
+
+
+
