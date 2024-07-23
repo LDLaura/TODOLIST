c34c206410472c8f10ba6111e6e207d0c59beb2c
LDLaura <despesselaura@yahoo.fr>
2024-07-23 15:36:59 +0200

    feat(chanteur): afficher chanteurs

--- views/bonus/chanteurs.php
+++ views/bonus/chanteurs.php
@@ -1,9 +1,22 @@
 <?php
 require_once '../../service/db_connect_music.php';
 
-$request = 'SELECT id, nom FROM artiste WHERE estchanteur = 1';
-$stmt = $db_connexion->query($request);
-$chanteurs = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$idArtiste = $_GET['id'];
+
+$request = 'SELECT nom, groupe.nom as "nom groupe", album.titre as "titre album" FROM artiste 
+            LEFT JOIN membregroupe ON membregroupe.idartiste = artiste.id
+            LEFT JOIN groupe ON groupe.id = membregroupe.id
+            LEFT JOIN album ON album.id = group.id
+            WHERE idArtiste = :id';
+$stmt = $db_connexion->prepare($request);
+$stmt->bindParam(':id', $idArtiste);
+
+$stmt->execute();
+$resultat = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+$chanteur = $resultat['artiste.nom'];
+$groupe = $resultat['groupe.nom'];
+$album = $resultat['album.titre'];
 
 ?>
 
@@ -16,7 +29,7 @@ $chanteurs = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BONUS : BDD MUSIC</title>
 
-    <link rel="stylesheet" href="../../style/bonus/filtreChanteur.css">
+    <link rel="stylesheet" href="../../style/bonus/chanteurs.css">
 </head>
 
 <body>
@@ -36,23 +49,22 @@ $chanteurs = $stmt->fetchAll(PDO::FETCH_ASSOC);
     </header>
 
     <main>
-        <?php
-        foreach ($chanteurs as $chanteur) {
-        ?>
+        <section class="main-content">
+            
             <div class="card">
-                <div class="header">
-                    <div class="icon">
+                    <div class="header">
+                        <span class="icon">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
                             <path d="M8.25 4.5a3.75 3.75 0 1 1 7.5 0v8.25a3.75 3.75 0 1 1-7.5 0V4.5Z" />
                             <path d="M6 10.5a.75.75 0 0 1 .75.75v1.5a5.25 5.25 0 1 0 10.5 0v-1.5a.75.75 0 0 1 1.5 0v1.5a6.751 6.751 0 0 1-6 6.709v2.291h3a.75.75 0 0 1 0 1.5h-7.5a.75.75 0 0 1 0-1.5h3v-2.291a6.751 6.751 0 0 1-6-6.709v-1.5A.75.75 0 0 1 6 10.5Z" />
                         </svg>
-                    </div>
-                    <?php echo '<a href="./chanteurs.php?id='.$chanteur['id'].'" class="title">' . $chanteur['nom'] . ' </a>' ?>
+                        </span>
+                        <p class="chanteur"><?= $chanteur ?></p>
+                        </div>
+                        <p class="content"><?= $groupe ?></p>
+                        <p class="content"><?= $album ?></p>
                 </div>
-            </div>
-        <?php
-        }
-        ?>
+        </section>
     </main>
 </body>
 
